8b51be1df3b764c848ea553c45a4ccf8912b62b8
Алексей Ипполитов <setgecko@gmail.com>
2022-08-12 17:50:55 +0300

    задали управление черепашкой

--- TurtleGame/Program.cs
+++ TurtleGame/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
 
 namespace TurtleGame
 {
@@ -10,6 +11,32 @@ namespace TurtleGame
     {
         static void Main(string[] args)
         {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            Turtle.PenUp();
+            while(true)
+            {
+                Turtle.Move(10);
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+             if (GraphicsWindow.LastKey == "Up" )
+            {
+                Turtle.Angle = 0;
+            }
+             else if (GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            }
+            else if (GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            }
+            else if (GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
         }
     }
 }
